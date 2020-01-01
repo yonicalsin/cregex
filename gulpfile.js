6010c6624b5580e6ec2db948d289127a459fd7aa
yonicb <yonicb20@gmail.com>
2020-01-01 10:03:36 -0500

    fix: 🐛 Remove sourcemaps for build task

--- gulpfile.js
+++ gulpfile.js
@@ -26,10 +26,10 @@ function build() {
         .pipe(tsProject())
         .js.pipe(gulp.dest("dist"))
         .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMaps: true }))
+        // .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist'));
+        // .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist'));
 }
 function message(cb, msg = `
     ========================
