d539176ee482a3cd7d4c5a6abde7935d832b9532
Yoni Calsin <yonicb20@gmail.com>
2020-03-02 08:30:27 -0500

    perf: ⚡️ Exported types for typescript !

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,17 @@
-declare function hello(name: string): string;
-export default hello;
+declare const cregexs: {
+   isEmail: () => RegExp;
+   isEmpty: () => RegExp;
+   isNoEmpty: () => RegExp;
+   blacklist: () => RegExp;
+   isAscii: () => RegExp;
+   isCreditCard: () => RegExp;
+   isHash: () => RegExp;
+   isHexColor: () => RegExp;
+   isLength: () => RegExp;
+   isLowercase: () => RegExp;
+   isMobilePhone: () => RegExp;
+   isSlug: () => RegExp;
+   isUrl: () => RegExp;
+   isUppercase: () => RegExp;
+};
+export default cregexs;
